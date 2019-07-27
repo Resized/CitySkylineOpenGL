5f846a3b67d9b461790aab81963f93c6954f2490
Resized <shai.yarden@gmail.com>
2019-07-27 04:16:22 +0300

    Added drawSeaWaves() function with animation in idle

--- Graphics/Func.h
+++ Graphics/Func.h
@@ -2,4 +2,3 @@
 void drawWindows(double x, double y, double w);
 void drawSingleBuildingReflection(double x, double y, double w);
 void drawWindowsReflection(double x, double y, double w, double windowWidth, double windowBuffer);
-void drawSingleBackBuilding(double x, double y, double w);
--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -8,11 +8,22 @@ const int NUM_OF_BUILDINGS = 100;
 const double PI = 3.14159;
 const int NUM_OF_WINDOWS = 100;
 const int NUM_OF_STARS = 100;
-double p = 0.2;
+const int NUM_OF_WAVES = 8;
+double waveSpeed = 0.001;
 
 typedef struct {
 	double x, y, size, speed;
 } STAR;
+
+typedef struct {
+	double y;
+	double size;
+	double sizeIndex;
+} WAVE;
+
+typedef struct {
+	double red, green, blue;
+} COLOR;
 
 typedef struct {
 	double x;
@@ -25,6 +36,7 @@ typedef struct {
 
 BUILDING buildings[NUM_OF_BUILDINGS];
 STAR stars[NUM_OF_STARS];
+WAVE waves[NUM_OF_WAVES];
 
 int buildingIndex = 0;
 
@@ -53,10 +65,17 @@ void init()
 
 	for (int i = 0; i < NUM_OF_STARS; i++)
 	{
-		stars[i].x = rand() % 1000 / 500.0 -1;
+		stars[i].x = rand() % 1000 / 500.0 - 1;
 		stars[i].y = rand() % 1000 / 1000.0;
 		stars[i].size = rand() % 1000 / 100000.0;
 		stars[i].speed = rand() % 1000 / 1000000.0 + 0.001;
+	}
+
+	for (int i = 0; i < NUM_OF_WAVES; i += 1)
+	{
+		waves[i].y = -i * i / (double)(NUM_OF_WAVES*NUM_OF_WAVES);
+		waves[i].size = (i + 1)*(i + 1) / (double)(NUM_OF_WAVES*NUM_OF_WAVES) + waves[i].y;
+		waves[i].sizeIndex = i;
 	}
 
 }
@@ -126,8 +145,8 @@ void drawSingleBuilding(double x, double y, double w)
 	glEnd();
 
 	// add shadow
-	double shadowWidth = w / 8;
-	glColor3d(0.1, 0, 0.1);
+	double shadowWidth = w / 3;
+	glColor3d(0.2, 0, 0.2);
 	glBegin(GL_POLYGON);
 	glVertex2d(x, y); // 1
 	glVertex2d(x - shadowWidth, y - shadowWidth);
@@ -155,11 +174,11 @@ void drawSingleBuildingReflection(double x, double y, double w)
 	glEnd();
 
 	// add shadow
-	double shadowWidth = w / 8;
+	double shadowWidth = w / 3;
 	glColor3d(0.1, 0, 0.1);
 	glBegin(GL_POLYGON);
 	glVertex2d(x, y); // 1
-	glVertex2d(x - shadowWidth, y+ shadowWidth);
+	glVertex2d(x - shadowWidth, y + shadowWidth);
 	glVertex2d(x - shadowWidth, 0);
 	glVertex2d(x, 0); // 1
 	glEnd();
@@ -171,9 +190,9 @@ void drawWindows(double x, double y, double w)
 	double windowWidth = (w - windowBuffer * (buildings[buildingIndex].windowsPerRow + 1)) / (buildings[buildingIndex].windowsPerRow);
 	double windowBuffer_x = windowBuffer * buildings[buildingIndex].windowStyle;
 	int k = 0, l = 0;
-	for (double i = x + windowBuffer; i < x + w - windowWidth; i += (windowWidth+ windowBuffer_x))
+	for (double i = x + windowBuffer; i < x + w - windowWidth; i += (windowWidth + windowBuffer_x))
 	{
-		for (double j = y - windowBuffer*2; j >= windowWidth; j -= (windowWidth + windowBuffer))
+		for (double j = y - windowBuffer * 2; j >= windowWidth; j -= (windowWidth + windowBuffer))
 		{
 			buildings[buildingIndex].windows[k][l] == 1 ? glColor3d(1, 1, 0) : glColor3d(0.2, 0.2, 0);
 			glBegin(GL_POLYGON);
@@ -198,7 +217,7 @@ void drawWindowsReflection(double x, double y, double w, double windowWidth, dou
 	double windowBuffer_x = windowBuffer * buildings[buildingIndex].windowStyle;
 	for (double i = x + windowBuffer; i < x + w - windowWidth; i += (windowWidth + windowBuffer_x))
 	{
-		for (double j = y + windowBuffer*2; j <= -windowWidth; j += (windowWidth + windowBuffer))
+		for (double j = y + windowBuffer * 2; j <= -windowWidth; j += (windowWidth + windowBuffer))
 		{
 			buildings[buildingIndex].windows[k][l] == 1 ? glColor3d(1, 1, 0) : glColor3d(0.2, 0.2, 0);
 			glBegin(GL_POLYGON);
@@ -239,11 +258,11 @@ void drawMoon()
 	glEnd();
 
 	glBegin(GL_POLYGON);
-	for (double x = -1; x < 1; x+=0.05)
+	for (double x = -1; x < 1; x += 0.05)
 	{
 		double moonColor = sin(x*PI) > 0.1 ? sin(x*PI) : 0.1;
 		glColor4d(moonColor, moonColor, moonColor, sin(x*PI));
-		glVertex2d(cos(x*PI)/12 - 0.8, sin(x*PI)/12 + 0.8); //1
+		glVertex2d(cos(x*PI) / 12 - 0.8, sin(x*PI) / 12 + 0.8); //1
 	}
 	glEnd();
 
@@ -274,9 +293,9 @@ void drawStreetLamps()
 		glBegin(GL_POLYGON);
 		glColor3d(0.5, 0.5, 0);
 		glVertex2d(x, step); //1
-		glVertex2d(x+step/10, step); //2
+		glVertex2d(x + step / 10, step); //2
 		glColor3d(0, 0, 0);
-		glVertex2d(x+ step/10, 0); //3
+		glVertex2d(x + step / 10, 0); //3
 		glVertex2d(x, 0); //4
 		glEnd();
 
@@ -320,7 +339,7 @@ void overlayReflection()
 {
 	double y;
 	// glColor4d(0, 0.2*fabs(1 + y), 0.6*fabs(1 + y), fabs(y/3) + 0.5);
-	glColor4d(0, 0, 0.4, 0.6);
+	glColor4d(0.1, 0, 0.4, 0.5);
 	glBegin(GL_POLYGON);
 	glVertex2d(-1, 0); //1
 	glVertex2d(1, 0); //2
@@ -330,6 +349,21 @@ void overlayReflection()
 	glEnd();
 }
 
+void drawSeaWaves()
+{
+	for (int i = 0; i < NUM_OF_WAVES; i++)
+	{
+		glBegin(GL_POLYGON);
+		glColor4d(0, 0, 0.2, 0.8);
+		glVertex2d(-1, waves[i].y); //1
+		glVertex2d(1, waves[i].y); //2
+		glColor4d(0, 0, 0, 0);
+		glVertex2d(1, waves[i].y - waves[i].size); //3
+		glVertex2d(-1, waves[i].y - waves[i].size); //4
+		glEnd();
+	}
+}
+
 void flipWindows(int index)
 {
 	int i = rand() % NUM_OF_WINDOWS;
@@ -337,21 +371,9 @@ void flipWindows(int index)
 	buildings[index].windows[i][j] ^= rand() % 1000 / 1000.0 > 0.1 ? 1 : 0;
 }
 
-void drawBackSkyline(double s_freq, double s_amp, double s_phase,
-	double c_freq, double c_amp, double c_phase,
-	double vertical_shift, double buildingWidth, double sampleFreq, double drawBuildingShift)
+void drawSingleBackBuilding(double x, double y, double w, COLOR color)
 {
-	double x, y;
-	for (x = -1 + drawBuildingShift; x <= 1; x += sampleFreq)
-	{
-		y = vertical_shift + fabs(s_amp * sin(s_phase + s_freq * x) + c_amp * cos(c_phase + c_freq * x));
-		drawSingleBackBuilding(x, y, buildingWidth);
-	}
-}
-
-void drawSingleBackBuilding(double x, double y, double w)
-{
-	glColor3d(0.2, 0, 0.2);
+	glColor3d(color.red, color.green, color.blue);
 	glBegin(GL_POLYGON);
 	glVertex2d(x, y); // 1
 	glVertex2d(x, 0);//2
@@ -361,13 +383,25 @@ void drawSingleBackBuilding(double x, double y, double w)
 
 	// reflection
 	y = -y;
-	glColor3d(0.2, 0, 0.2);
+	glColor3d(color.red, color.green, color.blue);
 	glBegin(GL_POLYGON);
 	glVertex2d(x, y); // 1
 	glVertex2d(x, 0);//2
 	glVertex2d(x + w, 0);//3
 	glVertex2d(x + w, y);//4
 	glEnd();
+}
+
+void drawBackSkyline(double s_freq, double s_amp, double s_phase,
+	double c_freq, double c_amp, double c_phase,
+	double vertical_shift, double buildingWidth, double sampleFreq, double drawBuildingShift, COLOR color)
+{
+	double x, y;
+	for (x = -1 + drawBuildingShift; x <= 1; x += sampleFreq)
+	{
+		y = vertical_shift + fabs(s_amp * sin(s_phase + s_freq * x) + c_amp * cos(c_phase + c_freq * x));
+		drawSingleBackBuilding(x, y, buildingWidth, color);
+	}
 }
 
 void display()
@@ -380,8 +414,25 @@ void display()
 	drawStars();
 	drawMoon();
 
-
 	double s_freq, s_amp, s_phase, c_freq, c_amp, c_phase, vertical_shift, buildingWidth, sampleFreq, drawBuildingShift;
+	COLOR color;
+
+	s_freq = PI * 10;
+	s_amp = 0.05;
+	s_phase = 0.3;
+	c_freq = PI * 0.3;
+	c_amp = 0.5;
+	c_phase = -0.1;
+	vertical_shift = 0.18;
+	buildingWidth = 0.098;
+	sampleFreq = 0.082;
+	drawBuildingShift = -0.04;
+	color.red = 0.1;
+	color.green = 0;
+	color.blue = 0.1;
+
+	drawBackSkyline(s_freq, s_amp, s_phase, c_freq, c_amp, c_phase, vertical_shift, buildingWidth, sampleFreq, drawBuildingShift, color); // Background Buildings #1
+
 
 	s_freq = PI * 20;
 	s_amp = 0.05;
@@ -393,9 +444,11 @@ void display()
 	buildingWidth = 0.092;
 	sampleFreq = 0.08;
 	drawBuildingShift = -0.05;
+	color.red = 0.15;
+	color.green = 0;
+	color.blue = 0.15;
 
-
-	drawBackSkyline(s_freq, s_amp, s_phase, c_freq, c_amp, c_phase, vertical_shift, buildingWidth, sampleFreq, drawBuildingShift);
+	drawBackSkyline(s_freq, s_amp, s_phase, c_freq, c_amp, c_phase, vertical_shift, buildingWidth, sampleFreq, drawBuildingShift, color); // Background Buildings #2
 
 	s_freq = PI * 5;
 	s_amp = 0.1;
@@ -408,7 +461,7 @@ void display()
 	sampleFreq = 0.15;
 	drawBuildingShift = 0.01;
 
-	drawSkyline(s_freq, s_amp, s_phase, c_freq, c_amp, c_phase, vertical_shift, buildingWidth, sampleFreq, drawBuildingShift); // Background Buildings
+	drawSkyline(s_freq, s_amp, s_phase, c_freq, c_amp, c_phase, vertical_shift, buildingWidth, sampleFreq, drawBuildingShift); // First Buildings Layer
 
 
 	s_freq = PI * 10;
@@ -422,7 +475,7 @@ void display()
 	sampleFreq = 0.11;
 	drawBuildingShift = 0.01;
 
-	drawSkyline(s_freq, s_amp, s_phase, c_freq, c_amp, c_phase, vertical_shift, buildingWidth, sampleFreq, drawBuildingShift);
+	drawSkyline(s_freq, s_amp, s_phase, c_freq, c_amp, c_phase, vertical_shift, buildingWidth, sampleFreq, drawBuildingShift);// Second Buildings Layer
 
 	s_freq = PI * 5;
 	s_amp = 0.2;
@@ -435,7 +488,7 @@ void display()
 	sampleFreq = 0.12;
 	drawBuildingShift = -0.01;
 
-	drawSkyline(s_freq, s_amp, s_phase, c_freq, c_amp, c_phase, vertical_shift, buildingWidth, sampleFreq, drawBuildingShift);
+	drawSkyline(s_freq, s_amp, s_phase, c_freq, c_amp, c_phase, vertical_shift, buildingWidth, sampleFreq, drawBuildingShift);// Third Buildings Layer
 
 	s_freq = PI * 5;
 	s_amp = 0.1;
@@ -443,7 +496,7 @@ void display()
 	c_freq = PI * 0.5;
 	c_amp = 0.05;
 	c_phase = -0.1;
-	vertical_shift = 0.05;
+	vertical_shift = 0.08;
 	buildingWidth = 0.21;
 	sampleFreq = 0.34;
 	drawBuildingShift = 0.08;
@@ -457,6 +510,7 @@ void display()
 	}
 	drawStreetLamps();
 	overlayReflection();
+	drawSeaWaves();
 
 	buildingIndex = 0;
 
@@ -468,7 +522,6 @@ void display()
 void idle()
 {
 	int i, j;
-	p += 0.1;
 
 	for (int i = 0; i < NUM_OF_STARS; i++)
 	{
@@ -478,6 +531,21 @@ void idle()
 		{
 			stars[i].x = 1;
 			stars[i].y = -1 + (rand() % 1000) / 500.0; // random number in (-1,1)
+		}
+	}
+
+	for (int i = 0; i < NUM_OF_WAVES; i++)
+	{
+		waves[i].sizeIndex += waveSpeed;
+		waves[i].y = -waves[i].sizeIndex * waves[i].sizeIndex / (double)(NUM_OF_WAVES*NUM_OF_WAVES);
+		waves[i].size = (waves[i].sizeIndex + 1)*(waves[i].sizeIndex + 1) / (double)(NUM_OF_WAVES*NUM_OF_WAVES) + waves[i].y;
+		if (waves[i].sizeIndex <= 0)
+		{
+			waves[i].sizeIndex = NUM_OF_WAVES;
+		}
+		else if (waves[i].sizeIndex > NUM_OF_WAVES)
+		{
+			waves[i].sizeIndex = 0;
 		}
 	}
 
